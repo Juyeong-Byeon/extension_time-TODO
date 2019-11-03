808f2fdf674b2a78b9e18ac0655c56cb400c5ae5
wndudqus <35381940+wndudqus@users.noreply.github.com>
2019-11-03 23:54:55 +0900

    slidebar 추가
    slidebar 추가 후 정적인 작업 완료
    //해야 할 것-> 타이머 구현, 버튼 기능 구현

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,6 @@
+let todoSeq=0;
+let todoID=0;
+
 function onclickStart(){
 
     alert("선택된 항목의 타이머를 시작합니다.");
@@ -14,28 +17,44 @@ function onclickDelete(){
 
 }
 
-let todoSeq=0;
-
-function endTimer(){
 
 
-    return 100;
+function endTimer(time){
+
+    let timeString="";
+    timeString=time+":00";
+
+    return timeString;
 }
+
+
 
 
 document.addEventListener('DOMContentLoaded',
  function(){
+    
     let btnStart=document.querySelector('#btnStart').addEventListener('click',onclickStart,false);
     let btnStop= document.querySelector('#btnStop').addEventListener('click',onclickStop,false);
     let btnDelete= document.querySelector('#btnDelete').addEventListener('click',onclickDelete,false);
     let todolist=document.querySelector('#todolist');
+    let todoTime=document.querySelector('#todoTime');
     
     
-    let todoText=document.querySelector('#todoinput').addEventListener('keypress',function (e) {
+    todoTime.addEventListener('input',
+    function currentRange(){
+        
+        document.querySelector('#currentRange').innerHTML="<label id='currentRange' class='label' for='todoTime'>TIME: "+this.value+" MIN"+"</label>";
+    
+    }
+    
+    );
+    
+    
+    let todoText=document.querySelector('#todoinput').addEventListener('keypress', function (e) {
         var key = e.which || e.keyCode;
         if (key === 13&&this.value) { // 13 is enter
             todoSeq+=1;
-            todolist.innerHTML+="<tr><td>"+todoSeq+"</td><td>"+this.value+"</td><td>"+endTimer()+"</td><td><input type='checkbox' name='selected' value='true'></td></tr>";
+            todolist.innerHTML+="<tr id='todo"+(todoID++)+"'><td>"+todoSeq+"</td><td>"+this.value+"</td><td>"+endTimer(todoTime.value)+"</td><td><input type='checkbox' name='selected' value='true'></td></tr>";
             this.value="";
 
         }
