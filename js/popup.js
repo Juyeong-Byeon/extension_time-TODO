bc56b37bea4081e88bfec244daefbca45aa6ac9c
wndudqus <35381940+wndudqus@users.noreply.github.com>
2019-11-15 23:49:54 +0900

    localstorage사용
    localstorage 사용

--- js/popup.js
+++ js/popup.js
@@ -3,29 +3,46 @@ let todoSeq=0;
 let todoID=0;
 let tempString='';
 let tempContentString='';
-let timers=[];
+let timers=[]; 
+
 
 
 let Timer={
 
-    id:'',
+    id:Number,
+    min:Number,
     seconds:Number,
     timerID: Number,
     todoContent: String,
+    startTime:Date,
+    endTime:String,
+    stoppedTime:Date,
+    pausedMin:Number,
 
-    initTimer: function(id,todoContent,seconds){
+
+
+    initTimer: function(id,todoContent,min,stoppedTime,pausedMin){
         this.id=id;
         this.todoContent=todoContent;
-        this.seconds=seconds;
-        this.timerID=setInterval(this.updateTimer.bind(this),1000);
+        this.min=min;
+        this.seconds=min*60;
+        this.startTime=new Date();
+        this.endTime=Math.floor(this.startTime.getHours()+this.min/60)+":"+(this.startTime.getMinutes()+this.min%60);
+        this.stoppedTime=stoppedTime,
+        this.pausedMin=pausedMin,
+        this.timerStart();
+        
     },
 
     timerStart: function(){
+        if(this.pausedMin!=0)clearInterval(this.timerID);
         this.timerID=setInterval(this.updateTimer.bind(this),1000);
+        console.log(this.stoppedTime);
     },
     timerStop: function(){
         clearInterval(this.timerID);
-        this.timerID=null;
+        this.stoppedTime=new Date();
+        this.timerID=setInterval(this.updateEndTime.bind(this),60000);
     },
     updateTimer: function (){
         let temp='';
@@ -52,9 +69,31 @@ let Timer={
         
         
     }
-    }
+    },
+    updateEndTime:function(){
+        let temp='#endTime'+this.id;
+        this.pausedMin++;
+        this.endTime=Math.floor(this.startTime.getHours()+(this.min+this.pausedMin)/60)+":"+(this.startTime.getMinutes()+(this.min+this.pausedMin)%60);
+        document.querySelector(temp).textContent=this.endTime;
+    },
    
+    getObjData:function(){
 
+        let datas={
+
+            "id":this.id,
+            "min":this.min,
+            "seconds":this.seconds,
+            "timerID": this.timerID,
+            "todoContent": this.todoContent,
+            "startTime":this.startTime,
+            "endTime":this.endTime,
+            "stoppedTime":this.startTime,
+            "pausedMin":this.pausedMin
+        };
+
+        return datas;
+    }
 }
 
 function onclickDelete(){//obj delete 구현 해주어야함. 
@@ -69,6 +108,7 @@ function onclickDelete(){//obj delete 구현 해주어야함.
             timers[i].timerStop();
             document.querySelector(tempString).remove();//tr을 지운다.
             delete timers[i];
+            localStorage.removeItem(i);
             countDelete++;
         }
 
@@ -118,24 +158,36 @@ function onclickStop(){
         if(document.querySelector(tempString)==undefined)continue;//지워졌을 경우 무시
         if(document.querySelector(tempString).checked==true){//각tr의 체크 박스를 확인해서 체크되어있으면
            timers[i].timerStop();
+           localStorage.setItem(todoID,JSON.stringify(timers[i].getObjData()));
         
         }
     }
 }
 
 
+function loadTodoList(){////////////////////////////////////////////////고치기
+let counter=0;
+for(let i=0;i<100;i++){
+    if(localStorage.getItem(i)==null)
+    continue;
+    
+    timers[counter]=localStorage.getItem(i);
+    counter++;
+}
+
+console.log(timers);
+}
 
 
 document.addEventListener('DOMContentLoaded',
  function(){
-    
     let btnStart=document.querySelector('#btnStart').addEventListener('click',onclickStart,false);
     let btnStop= document.querySelector('#btnStop').addEventListener('click',onclickStop,false);
     let btnDelete= document.querySelector('#btnDelete').addEventListener('click',onclickDelete,false);
     let todolist=document.querySelector('#todolist');
     let todoTime=document.querySelector('#todoTime');
-    
-    
+    if(localStorage.timers)
+        console.log(localStorage.timers);
     todoTime.addEventListener('input',
     function currentRange(){
         
@@ -144,21 +196,29 @@ document.addEventListener('DOMContentLoaded',
     }
     
     );
-    
+    loadTodoList();
 
 document.querySelector('#todoinput').addEventListener('keypress', function (e) {
         var key = e.which || e.keyCode;
         if (key === 13&&this.value) { // 13 is enter
             todoSeq+=1;
-            todolist.innerHTML+="<tr id='todo"+(todoID)+"'><td id='todoSeq"+todoSeq+"'>"+todoSeq+"</td><td id='do"+todoID+"'>"+this.value+"</td><td id='timerView"+todoID+"'>00:00</td><td><input id='todoChecked"+todoID+"' type='checkbox' name='selected' value='true'></td></tr>";
             timers[todoID]=Object.create(Timer);
-            timers[todoID].initTimer(todoID,this.value,todoTime.value*60);
+            timers[todoID].initTimer(todoID,this.value, todoTime.value,null,null);
+            todolist.innerHTML+="<tr id='todo"+(todoID)+"'><td id='todoSeq"+todoSeq+"'>"+todoSeq+"</td><td id='do"+todoID+"'>"+timers[todoID].todoContent+"</td><td>"+timers[todoID].startTime.getHours()+":"+timers[todoID].startTime.getMinutes()+"</td><td id='endTime"+todoID+"'>"+timers[todoID].endTime+"</td><td id='timerView"+todoID+"'>00:00</td><td><input id='todoChecked"+todoID+"' type='checkbox' name='selected' value='true'></td></tr>";
+            localStorage.setItem(todoID,JSON.stringify(timers[todoID].getObjData()));
             todoID++;
             this.value="";
-
+            
         }
         
     });
+
+
+
 },false);
+
+window.addEventListener("beforeunload", function (event) {
+    
+  });
 
 
